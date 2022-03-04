e6c42e9d59e6b8d3b9a32d39134b1b3c1f7b7d28
juliett-wanjiru <wanjiruujuliet@gmail.com>
2022-03-04 21:43:35 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var fullname= "Judy Wanjiru"
+    var age= 19
+    var phonenumber=254797239759
+    var weight=59
+    val citizenkenyan:Boolean= true
+    val citizennotkenyan:Boolean =false
+    println(fullname)
+    println(age)
+    println(phonenumber)
+    println(weight)
+    println(citizenkenyan)
+    println(citizennotkenyan)
+}
+
+
